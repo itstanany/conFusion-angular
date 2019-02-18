cfb19243bbef8f63c386b40ffa992450d2809880
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-02-18 15:16:30 +0200

    Single Page Applications Part2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 // importing the Dish class as interface
 import { Dish } from '../shared/dish';
@@ -8,11 +11,17 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-    @Input()
   dish: Dish;
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    // i don't know why ther is a plus + sign at the begging of the assignment expression
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
-
+  gotBack(): void {
+    this.location.back();
+  }
 }
