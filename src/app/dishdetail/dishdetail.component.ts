0e2f16c85d84453de7ad16573342504ef99da675
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-02-24 14:55:32 +0200

    RxJs Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+// using rxJs
+import { switchMap } from 'rxjs/operators';
 // importing the Dish class as interface
 import { Dish } from '../shared/dish';
 @Component({
@@ -12,15 +13,23 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
+  dishIds: string [];
+  prev: string;
+  next: string;
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
     // i don't know why ther is a plus + sign at the begging of the assignment expression
-    const id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id)
-    .subscribe((dishParameter) => this.dish = dishParameter);
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
   gotBack(): void {
     this.location.back();
