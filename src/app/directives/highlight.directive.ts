795cb3d93b2067cc3c614ce4d72876a374fa1e43
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-03-02 18:57:33 +0200

    Animations Part 1

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -8,7 +8,7 @@ export class HighlightDirective {
   constructor(private el: ElementRef,
     private renderer: Renderer2) { }
 
-    @HostListener('mouseenter') onmouseenyet(){
+    @HostListener('mouseenter') onmouseenyet() {
        this.renderer.addClass(this.el.nativeElement, 'highlight');
     }
     @HostListener('mouseleave') onmouseleave() {
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -10,12 +10,26 @@ import { switchMap } from 'rxjs/operators';
 // importing the Dish class as interface
 import { Dish } from '../shared/dish';
 import { from } from 'rxjs';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
@@ -25,9 +39,10 @@ export class DishdetailComponent implements OnInit {
   next: string;
   dishcopy: Dish;
   comform: FormGroup; // the actual form
+  @ViewChild('cform') comformDirective;
   comment: Comment;
   scomform: Comment; // the final form to submit
-  @ViewChild('cform') comformDirective;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -64,8 +79,8 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     // i don't know why ther is a plus + sign at the begging of the assignment expression
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(params['id']); }))
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
     errmess => this.errMess = <any>errmess);
   }
   setPrevNext(dishId: string) {
